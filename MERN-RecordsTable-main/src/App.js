bd53e9f7face3ae112e3d0b23cecc7b8a71addab
Joshiakshaj <129145776+Joshiakshaj@users.noreply.github.com>
2023-11-10 11:11:13 +0530

    Add files via upload

---
+++ MERN-RecordsTable-main/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Table from './components/Table';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+
+function App() {
+  return (
+    <div>
+      <Table />
+    </div>
+  );
+}
+
+export default App;
